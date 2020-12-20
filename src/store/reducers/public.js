1368dc2719f9a07253a0d7fe103295e806a53de3
wkk <1754393348@qq.com>
2020-12-21 00:15:43 +0800

    2020.12.21

--- src/store/reducers/public.js
+++ src/store/reducers/public.js
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2020-12-15 12:57:43
- * @LastEditTime: 2020-12-18 17:28:26
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /wxfishing/src/store/reducers/public.js
- */
 import { handleActions } from 'redux-actions';
 //这里把types里的函数名引入 注意相对路径
 import { SETID, COMMIT } from '../types/public';
@@ -15,9 +7,6 @@ const defaultState = {
   id: null,
   token: null,
   status: null,
-  payInfo: null,
-  masterPhone: null,
-  order: null,
 }
 export default handleActions({
   [SETID] (state, action) {
@@ -26,9 +15,12 @@ export default handleActions({
   },
 
   [COMMIT] (state, action) {
+    console.log('state, action',state, action)
     for(const key in action.payload){
+      console.log('action.payload***',key,action.payload)
       state[key] = action.payload[key]
     }
+    console.log('state--',state)
     return state
   }
 }, defaultState)
